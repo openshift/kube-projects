eb7baf0cfded4e4239bfabaad62b0c130fe9fd48
deads2k <deads@redhat.com>
2016-10-20 16:33:55 -0400

    add project cache

---
+++ pkg/project/auth/reviewer.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	kapi "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/apis/rbac"
+	"k8s.io/kubernetes/pkg/auth/authorizer"
+	rbacauthorizer "k8s.io/kubernetes/plugin/pkg/auth/authorizer/rbac"
+)
+
+// Reviewer performs access reviews for a project by name
+type Reviewer interface {
+	Review(name string) ([]rbac.Subject, error)
+}
+
+// reviewer performs access reviews for a project by name
+type reviewer struct {
+	subjectAccessEvaluator rbacauthorizer.SubjectAccessEvaluator
+}
+
+// NewReviewer knows how to make access control reviews for a resource by name
+func NewReviewer(subjectAccessEvaluator rbacauthorizer.SubjectAccessEvaluator) Reviewer {
+	return &reviewer{
+		subjectAccessEvaluator: subjectAccessEvaluator,
+	}
+}
+
+// Review performs a resource access review for the given resource by name
+func (r *reviewer) Review(name string) ([]rbac.Subject, error) {
+	action := authorizer.AttributesRecord{
+		Verb:            "get",
+		Namespace:       name,
+		APIGroup:        kapi.GroupName,
+		Resource:        "namespaces",
+		Name:            name,
+		ResourceRequest: true,
+	}
+	return r.subjectAccessEvaluator.AllowedSubjects(action)
+}
