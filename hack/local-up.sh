e1f794b5e01d99a5b523c68af944f4edcbd5822a
deads2k <deads@redhat.com>
2016-12-16 13:41:33 -0500

    proof of integration

--- hack/local-up.sh
+++ hack/local-up.sh
@@ -9,11 +9,11 @@ ${OS_ROOT}/_output/local/bin/linux/amd64/project-server \
   --requestheader-username-headers=X-Remote-User \
   --requestheader-group-headers=X-Remote-Group \
   --requestheader-extra-headers-prefix=X-Remote-Extra- \
-  --requestheader-client-ca-file=/var/run/kubernetes/auth-proxy-client-ca.crt \
+  --requestheader-client-ca-file=/var/run/kubernetes/request-header-ca.crt \
   --requestheader-allowed-names=system:auth-proxy \
   --auth-user=project-server \
   --server-user=project-server \
   --kubeconfig=${OS_ROOT}/test/artifacts/local-secure-anytoken-server.kubeconfig \
-  --client-ca-file=/var/run/kubernetes/complete-client-ca-bundle.crt \
+  --client-ca-file=/var/run/kubernetes/client-ca.crt \
   --tls-ca-file=/var/run/kubernetes/apiserver.crt \
   --secure-port=8445
