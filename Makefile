90fd84ffab89bb7e158f79032f8ed1d1835f9004
David Eads <deads@redhat.com>
2017-10-25 08:34:31 -0400

    script

--- Makefile
+++ Makefile
@@ -5,6 +5,14 @@ build:
 	go build -o _output/bin/kube-projects github.com/openshift/kube-projects/cmd/project-server
 .PHONY: build
 
+build-image: build
+	hack/build-image.sh
+.PHONY: build-image
+
+verify:
+	go test github.com/openshift/kube-projects/pkg/...
+.PHONY: verify
+
 clean:
 	rm -rf _output
 .PHONY: clean
--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -16,3 +16,15 @@ rm -rf ./vendor/k8s.io/apiserver/vendor/github.com/emicklei
 
 #  our package and apiserver have to agree on github.com/spf13/pflag or we fail to build.  Yours, mine, and ours fail!
 rm -rf ./vendor/k8s.io/apiserver/vendor/github.com/spf13/pflag
+
+#  our package and apiserver have to agree on github.com/golang/glog or we fail to run
+rm -rf ./vendor/github.com/openshift/generic-admission-server/vendor/github.com/golang/glog
+rm -rf ./vendor/github.com/openshift/generic-admission-server/vendor/github.com/golang/glog
+
+#  all the k8s.io/* need to agree or we end up failing.  Yours, mine, and ours fail!
+rm -rf ./vendor/github.com/openshift/generic-admission-server/vendor/k8s.io/apimachinery
+rm -rf ./vendor/github.com/openshift/generic-admission-server/vendor/k8s.io/api
+rm -rf ./vendor/github.com/openshift/generic-admission-server/vendor/k8s.io/client-go
+
+# relies on an upstream pull.
+# glide vc --keep-full k8s.io/code-generator/**
